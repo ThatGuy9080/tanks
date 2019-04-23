9762f4553a69cb7adf25fb71a732cbe575eafeb0
Miles <spamacount120@gmail.com>
2019-04-23 08:57:25 -0500

    Made it work with the new stuff

--- sketch.js
+++ sketch.js
@@ -1,35 +1,31 @@
-let SPEED = 5;
+let TANKSPEED = 5;
+let BULLETSPEED = 10;
+let BULLETSIZE = 5;
+let TANKWIDTH = 100;
+let TANKLENGTH = 50;
+let COOLDOWN = 10;
+let BULLETCOUNT=50;
+let CANVASWIDTH = 500;
+let CANVASHEIGHT = 500;
 let Tanks = [];
-let Shooter = [];
+
+
 
 function setup() {
-  createCanvas(500, 500);
- angleMode(DEGREES);
+  createCanvas(CANVASWIDTH, CANVASHEIGHT);
+  angleMode(DEGREES);
   rectMode(CENTER);
-  for(i=0;i<1;i++){
-    Tanks[i]= new Rectangle(75+(100*i),50,0)
+  for (i = 0; i < 1; i++) {
+    Tanks[i] = new Tank(75 + (100 * i), 50, 0);
   }
-  for(i=0;i<1;i++){
-      Shooter[i]= new Bullet(0,0,42)
-}
 }
 
 
 
- function draw() {
-  background(0); 
-   for (i=0;i<Tanks.length;i++){
+function draw() {
+  background(0);
+  for (let i in Tanks) {
     Tanks[i].update();
     Tanks[i].draw();
-   }
-  
-
-   Shooter[0].update();
-   Shooter[0].draw();
-   if (keyCode === 74) {
-     Shooter[0].createBullet(Tanks[0].check()[0],Tanks[0].check()[1],Tanks[0].check()[2]);
-    }
-   else {
-   return false;
-   }
- }
+  }
+}
